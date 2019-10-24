ea7fcd5794b5529a11069d17edacacb5845d89df
Trevor Osterman <trevormosterman@gmail.com>
2019-10-23 20:59:10 -0700

    search bar implemented

--- src/server.js
+++ src/server.js
@@ -1,7 +1,6 @@
 const app = require("./app");
 const { PORT, DB_URL } = require("./config");
 const knex = require("knex");
-const EventsService = require("./events/events-service.js");
 
 const db = knex({
   client: "pg",
