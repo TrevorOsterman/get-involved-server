d50d0996480a689910a54eba8c818200650e2391
Trevor Osterman <trevormosterman@gmail.com>
2019-10-24 21:23:13 -0700

    cors troubleshooting

--- src/server.js
+++ src/server.js
@@ -2,14 +2,13 @@ const app = require("./app");
 const { PORT, DB_URL } = require("./config");
 const knex = require("knex");
 const cors = require("cors");
-
+app.use(cors());
 const db = knex({
   client: "pg",
   connection: DB_URL
 });
 
 app.set("db", db);
-app.use(cors());
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`);
