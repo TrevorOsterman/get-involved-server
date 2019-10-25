67c7399058ec05ab560d2e4546fc7b3ba3b1a9e6
Trevor Osterman <trevormosterman@gmail.com>
2019-10-24 22:18:37 -0700

    deploying server

--- src/app.js
+++ src/app.js
@@ -1,13 +1,14 @@
 require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 const helmet = require("helmet");
 const { NODE_ENV } = require("./config");
 const { CLIENT_ORIGIN } = require("./config");
 const eventsRouter = require("./events/events-router.js");
 
 const app = express();
-
+app.use(cors());
 const morganOption = NODE_ENV === "production" ? "tiny" : "common";
 
 app.use(morgan(morganOption));
