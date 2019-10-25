2157725e390efd4a54820664b4971f469551db14
Trevor Osterman <trevormosterman@gmail.com>
2019-10-24 22:37:19 -0700

    live database deployed

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || "development",
   REACT_APP_API_BASE_URL:
-    "http://localhost:8000" || process.env.REACT_APP_API_BASE_URL,
+    process.env.REACT_APP_API_BASE_URL || "http://localhost:8000",
   DATABASE_URL:
     process.env.DATABASE_URL ||
     "postgresql://TrevorOsterman@localhost/getInvolved",
