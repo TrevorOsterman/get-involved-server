41fc00836128a432b979b3ad744294a76ca9d271
Trevor Osterman <trevormosterman@gmail.com>
2019-10-25 18:24:26 -0700

    removed primary key

--- sql-scripts/seed-events.sql
+++ sql-scripts/seed-events.sql
@@ -1,5 +1,5 @@
-INSERT INTO events (title, event_date, city, state, description)
+INSERT INTO events (eventid, title, event_date, city, state, description)
 values
-('Serving Stable Servers', '2019/10/31', 'Tempe', 'AZ', 'test'),
-('Serving Stable Servers 2', '2019/10/31', 'Phoenix', 'AZ', 'test'),
-('Oh My, Please Work', '2019/12/12', 'Mesa', 'AZ', 'please please work');
+('1', 'Serving Stable Servers', '2019/10/31', 'Tempe', 'AZ', 'test'),
+('2','Serving Stable Servers 2', '2019/10/31', 'Phoenix', 'AZ', 'test'),
+('3','Oh My, Please Work', '2019/12/12', 'Mesa', 'AZ', 'please please work');
