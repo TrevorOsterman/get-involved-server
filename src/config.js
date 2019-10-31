84f7d0bdbe84c6b29fff100ff02e8c2ccd84024c
Trevor Osterman <trevormosterman@gmail.com>
2019-10-31 09:20:22 -0700

    DB connections update

--- src/config.js
+++ src/config.js
@@ -5,6 +5,6 @@ module.exports = {
     process.env.REACT_APP_API_BASE_URL || "http://localhost:8000",
   DATABASE_URL:
     process.env.DATABASE_URL ||
-    "postgresql://TrevorOsterman@localhost/getInvolved",
+    "postgres://hdphclavnckfrs:44ebd98adc254aac7fe39f18b4a8fa2998bb97f9a88ea66030f22ab48b702aa3@ec2-184-73-169-163.compute-1.amazonaws.com:5432/d46isq1b39p75a",
   CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || "http://localhost:3000"
 };
